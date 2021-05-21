c3d5ee84b8fce878bd03905a96660c1f99aafe08
zoeliauh <ibm801227@hotmail.com>
2021-05-21 11:22:44 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by woanjwu liauh on 2021/5/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
