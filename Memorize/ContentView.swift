d7b9fe4abf670ec9dabe9a8cd0f632e0002736c9
zoeliauh <ibm801227@hotmail.com>
2021-05-21 14:30:50 +0800

    Stanford lesson 1 : Memorize app

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        return HStack(content: {
+            ForEach(0..<4, content: { index in
+                ZStack(content: {
+                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                    Text("👻")
+                })
+            })
+        })
             .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
