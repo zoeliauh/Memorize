777a382e8e1774ab3bae328db293b0e02297e3b0
zoeliauh <ibm801227@hotmail.com>
2021-05-21 15:10:06 +0800

    more understandable and set up different type either face up or face down.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,11 +11,7 @@ struct ContentView: View {
     var body: some View {
         HStack {
             ForEach(0..<4) { index in
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                    Text("👻")
-                }
+                CardView(isFaceUp: false)
             }
         }
             .padding()
@@ -24,7 +20,22 @@ struct ContentView: View {
     }
 }
 
-
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+          
+        }
+    }
+}
 
 
 
